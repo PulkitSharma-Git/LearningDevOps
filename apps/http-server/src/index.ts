ee02e1b7a2f471efa8b1ebe15dba50da1e3ac5dd
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-03-23 19:35:44 +0530

    Next not conn to db

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { client } from "@repo/db/client"
+
+
+const app  = express();
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("hi there")
+})
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await client.user.create({
+        data:{
+            username,
+            password 
+
+        }
+    })
+
+    res.json({
+        message: "Sign Up Successfull",
+        id: user.id
+    })
+
+
+})
+
+app.listen(3002)
